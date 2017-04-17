72c62b0e17b0eb3d1ec1ee19db3c04b288400aba
Brent Ruhle <Greedfeed@gmail.com>
2017-04-17 15:15:32 -0400

    Major overhaul of the image naming convention, as well as completing basic POV screen

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
 //listen for keyboard input
 window.addEventListener("keydown", move_player, false);
 
-var canvas = document.getElementById('world_map');
-if (canvas.getContext) {
-	var context = canvas.getContext('2d');
+var overhead_map_canvas = document.getElementById('overhead_map');
+if (overhead_map_canvas.getContext) {
+	var overhead_map_context = overhead_map_canvas.getContext('2d');
+}
+
+var player_pov_canvas = document.getElementById('player_pov');
+if (player_pov_canvas.getContext) {
+	var player_pov_context = player_pov_canvas.getContext('2d');
 }
 
 //Map with values
 var map = [
 	['1', '0', '0', '1', '0', '0', '0', '0', '0', '1', '1', '0', '0'],
-	['0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0'],
+	['0', '0', '1', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0'],
 	['0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0'],
 	['0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0'],
-	['0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0'],
+	['0', '0', '0', '1', '0', '0', '1', '0', '1', '0', '0', '0', '0'],
 	['0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0'],
-	['0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0'],
+	['0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0'],
 	['0', '0', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0'],
 	['0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0'],
 	['0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '0', '1', '0'],
@@ -25,24 +30,34 @@ var map = [
 var player_pos_x = 6;
 var player_pos_y = 10;
 //Players starting orientation
-var compassOLD = ['north', 'east', 'south', 'west'];
 var compass = { 'north' : 0, 'east' : 90, 'south': 180, 'west': 270 };
-
 
 orientation = compass['north'];
 
+var cone_of_vision = {
+	'col_-2_row_3':'', 'col_-1_row_3':'', 'col_2_row_3':'', 'col_1_row_3':'', 'col_0_row_3':'',
+	'col_-2_row_2':'', 'col_-1_row_2':'', 'col_2_row_2':'', 'col_1_row_2':'', 'col_0_row_2':'',
+	'col_-2_row_1':'', 'col_-1_row_1':'', 'col_2_row_1':'', 'col_1_row_1':'', 'col_0_row_1':'',
+					'col_-1_row_0':'', 			   			'col_1_row_0':''
+};
 
+init_imgs(cone_of_vision);
 
 function create_world() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	draw_grid();
-	draw_player(player_pos_x, player_pos_y, orientation);		
+	draw_map();
+	draw_player(player_pos_x, player_pos_y, orientation);	
+	draw_pov();	
 }
 
 
-function draw_grid() {
-	context.strokeStyle = '#757374';
+function draw_map() {
+	//Clear Contents of Previous Map Canvas
+	overhead_map_context.clearRect(0, 0, overhead_map_canvas.width, overhead_map_canvas.height);
+	//Give Map a White Background
+	overhead_map_context.fillStyle = '#FFFFFF';
+	overhead_map_context.fillRect(0, 0, overhead_map_canvas.width, overhead_map_canvas.height);
+
+	overhead_map_context.strokeStyle = '#757374';
 
 	var current_row = 0;
 	var current_column = 0;
@@ -50,11 +65,11 @@ function draw_grid() {
 	for (var i=0; i < map.length; i++) {
 		for (var j=0; j < map[i].length; j++) {
 			if (map[i][j] == 1) {
-				context.fillStyle = '#757374';
+				overhead_map_context.fillStyle = '#757374';
 
-				context.fillRect(current_row, current_column, 10, 10);
+				overhead_map_context.fillRect(current_row, current_column, 10, 10);
 			} else {
-				context.strokeRect(current_row, current_column, 10, 10);
+				overhead_map_context.strokeRect(current_row, current_column, 10, 10);
 			}
 
 			current_row = current_row + 10;
@@ -65,28 +80,28 @@ function draw_grid() {
 }
 
 function draw_player(x, y, orientation) {
-	context.beginPath();
+	overhead_map_context.beginPath();
 
 	switch(orientation) {
 		case 0:
-			context.moveTo((x * 10 + 5), (y * 10 + 2)); //compass point
-			context.lineTo((x * 10 + 2), (y * 10 + 8));
-			context.lineTo((x * 10 + 8), (y * 10 + 8));
+			overhead_map_context.moveTo((x * 10 + 5), (y * 10 + 2)); //compass point
+			overhead_map_context.lineTo((x * 10 + 2), (y * 10 + 8));
+			overhead_map_context.lineTo((x * 10 + 8), (y * 10 + 8));
 			break;
 		case 90:
-			context.moveTo((x * 10 + 8), (y * 10 + 5)); //compass point
-			context.lineTo((x * 10 + 2), (y * 10 + 2));
-			context.lineTo((x * 10 + 2), (y * 10 + 8));
+			overhead_map_context.moveTo((x * 10 + 8), (y * 10 + 5)); //compass point
+			overhead_map_context.lineTo((x * 10 + 2), (y * 10 + 2));
+			overhead_map_context.lineTo((x * 10 + 2), (y * 10 + 8));
 			break;
 		case 180:
-			context.moveTo((x * 10 + 5), (y * 10 + 8)); //compass point
-			context.lineTo((x * 10 + 2), (y * 10 + 2));
-			context.lineTo((x * 10 + 8), (y * 10 + 2));
+			overhead_map_context.moveTo((x * 10 + 5), (y * 10 + 8)); //compass point
+			overhead_map_context.lineTo((x * 10 + 2), (y * 10 + 2));
+			overhead_map_context.lineTo((x * 10 + 8), (y * 10 + 2));
 			break;
 		case 270:
-			context.moveTo((x * 10 + 2), (y * 10 + 5)); //compass point
-			context.lineTo((x * 10 + 8), (y * 10 + 2));
-			context.lineTo((x * 10 + 8), (y * 10 + 8));
+			overhead_map_context.moveTo((x * 10 + 2), (y * 10 + 5)); //compass point
+			overhead_map_context.lineTo((x * 10 + 8), (y * 10 + 2));
+			overhead_map_context.lineTo((x * 10 + 8), (y * 10 + 8));
 			break;
 		
 		default:
@@ -96,9 +111,9 @@ function draw_player(x, y, orientation) {
 			break;
 	}
 
-	context.fillStyle = '#B6091A';
-	context.fill();
-	context.closePath();
+	overhead_map_context.fillStyle = '#B6091A';
+	overhead_map_context.fill();
+	overhead_map_context.closePath();
 }
 
 function turn_player(move) {
@@ -176,17 +191,110 @@ function move_player(e) {
 	switch(e.keyCode) {
 		case 37:
 			player_move = 'left';
+			turn_player(player_move);
+
 			break;
 		case 38:
 			player_move = 'forward';
+			turn_player(player_move);
+
 			break;
 		case 39:
 			player_move = 'right';
+			turn_player(player_move);
+
 			break;
 		case 40:
 			player_move = 'backward';
+			turn_player(player_move);
+
 			break;
 	}
 
-	turn_player(player_move);
+}
+
+
+
+
+
+function draw_pov() {
+	var x = player_pos_x;
+	var y = player_pos_y;
+
+	//Clear Contents of Previous Map Canvas
+	player_pov_context.clearRect(0, 0, player_pov_canvas.width, player_pov_canvas.height);
+	//Give Map a White Background
+	player_pov_context.fillStyle = '#FFFFFF';
+	player_pov_context.fillRect(0, 0, player_pov_canvas.width, player_pov_canvas.height);
+
+	switch(orientation) {
+		case 0:
+			cone_of_vision = {
+				'col_-2_row_3':[(y - 3), (x - 2)], 'col_-1_row_3':[(y - 3), (x - 1)], 'col_2_row_3':[(y - 3), (x + 2)], 'col_1_row_3':[(y - 3), (x + 1)], 'col_0_row_3':[(y - 3), (x)],
+				'col_-2_row_2':[(y - 2), (x - 2)], 'col_-1_row_2':[(y - 2), (x - 1)], 'col_2_row_2':[(y - 2), (x + 2)], 'col_1_row_2':[(y - 2), (x + 1)], 'col_0_row_2':[(y - 2), (x)],
+				'col_-2_row_1':[(y - 1), (x - 2)], 'col_-1_row_1':[(y - 1), (x - 1)], 'col_2_row_1':[(y - 1), (x + 2)], 'col_1_row_1':[(y - 1), (x + 1)], 'col_0_row_1':[(y - 1), (x)],
+												   'col_-1_row_0':[(y - 0), (x - 1)], 									'col_1_row_0':[(y - 0), (x + 1)]
+			};
+			break;
+		case 90:
+			cone_of_vision = {
+				'col_-2_row_3':[(y - 2), (x + 3)], 'col_-1_row_3':[(y - 1), (x + 3)], 'col_2_row_3':[(y + 2), (x + 3)], 'col_1_row_3':[(y + 1), (x + 3)], 'col_0_row_3':[(y), (x + 3)],
+				'col_-2_row_2':[(y - 2), (x + 2)], 'col_-1_row_2':[(y - 1), (x + 2)], 'col_2_row_2':[(y + 2), (x + 2)], 'col_1_row_2':[(y + 1), (x + 2)], 'col_0_row_2':[(y), (x + 2)],
+				'col_-2_row_1':[(y - 2), (x + 1)], 'col_-1_row_1':[(y - 1), (x + 1)], 'col_2_row_1':[(y + 2), (x + 1)], 'col_1_row_1':[(y + 1), (x + 1)], 'col_0_row_1':[(y), (x + 1)],
+												   'col_-1_row_0':[(y - 1), (x + 0)], 									'col_1_row_0':[(y + 1), (x + 0)]
+			};
+			break;
+		case 180:
+			cone_of_vision = {
+				'col_-2_row_3':[(y + 3), (x + 2)], 'col_-1_row_3':[(y + 3), (x + 1)], 'col_2_row_3':[(y + 3), (x + 2)], 'col_1_row_3':[(y + 3), (x - 1)], 'col_0_row_3':[(y + 3), (x)],
+				'col_-2_row_2':[(y + 2), (x + 2)], 'col_-1_row_2':[(y + 2), (x + 1)], 'col_2_row_2':[(y + 2), (x + 2)], 'col_1_row_2':[(y + 2), (x - 1)], 'col_0_row_2':[(y + 2), (x)],
+				'col_-2_row_1':[(y + 1), (x + 2)], 'col_-1_row_1':[(y + 1), (x + 1)], 'col_2_row_1':[(y + 1), (x + 2)], 'col_1_row_1':[(y + 1), (x - 1)], 'col_0_row_1':[(y + 1), (x)],
+												   'col_-1_row_0':[(y + 0), (x + 1)], 									'col_1_row_0':[(y + 0), (x - 1)]
+			};
+			break;
+		case 270:
+			cone_of_vision = {
+				'col_-2_row_3':[(y + 2), (x - 3)], 'col_-1_row_3':[(y + 1), (x - 3)], 'col_2_row_3':[(y - 2), (x - 3)], 'col_1_row_3':[(y - 1), (x - 3)], 'col_0_row_3':[(y), (x - 3)],
+				'col_-2_row_2':[(y + 2), (x - 2)], 'col_-1_row_2':[(y + 1), (x - 2)], 'col_2_row_2':[(y - 2), (x - 2)], 'col_1_row_2':[(y - 1), (x - 2)], 'col_0_row_2':[(y), (x - 2)],
+				'col_-2_row_1':[(y + 2), (x - 1)], 'col_-1_row_1':[(y + 1), (x - 1)], 'col_2_row_1':[(y - 2), (x - 1)], 'col_1_row_1':[(y - 1), (x - 1)], 'col_0_row_1':[(y), (x - 1)],
+												   'col_-1_row_0':[(y + 1), (x - 0)], 									'col_1_row_0':[(y - 1), (x - 0)]
+			};
+			break;
+		default:
+			break;
+	}
+	
+	for (wall_location in cone_of_vision) {
+
+		var wall_y = cone_of_vision[wall_location][0];
+		var wall_x = cone_of_vision[wall_location][1];
+
+		
+		if (map[wall_y] != null && map[wall_y][wall_x] != null && wall_y >= 0 && wall_x >= 0 ) {
+			if (map[wall_y][wall_x] == 1 ) {
+				place_wall(wall_location);
+			}
+		} else {
+			place_wall(wall_location);
+		}
+
+		
+	}
+}
+
+function init_imgs(imgs) {
+	for (img in imgs) {
+		var wall_img = new Image();
+		wall_img.src = 'img/'+img+'.png';
+	}
+}
+
+function place_wall(wall_location) {
+	var wall_img = new Image();
+	wall_img.onload = function() {
+		var	y = (player_pov_canvas.height - this.height) * 0.5;
+		player_pov_context.drawImage(wall_img, 0, y);
+	}
+
+	wall_img.src = 'img/'+wall_location+'.png';
 }
